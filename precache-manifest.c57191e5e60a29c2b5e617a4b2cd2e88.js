21953fa98dfb47fb5b3e3e511cfdf50682de3186
nbrown0000 <brown.nathanjames@gmail.com>
2020-04-25 11:23:11 +1000

    Updates

---
+++ precache-manifest.c57191e5e60a29c2b5e617a4b2cd2e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f39f1510e36097449a0b6b3842baf8",
+    "url": "/movierecommender/index.html"
+  },
+  {
+    "revision": "aca8b4c1da9c01a41e99",
+    "url": "/movierecommender/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "fcc3f39c23200cfeb777",
+    "url": "/movierecommender/static/css/main.f06c5f25.chunk.css"
+  },
+  {
+    "revision": "aca8b4c1da9c01a41e99",
+    "url": "/movierecommender/static/js/2.5edc6743.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movierecommender/static/js/2.5edc6743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc3f39c23200cfeb777",
+    "url": "/movierecommender/static/js/main.a91d0f6d.chunk.js"
+  },
+  {
+    "revision": "d3a5f6badb4169233478",
+    "url": "/movierecommender/static/js/runtime-main.f7be4eef.js"
+  }
+]);
